472d8b824b5e369a94b69c74ebdd4363fdbea08a
MJ20037 <mriduljoshi836@gmail.com>
2025-06-29 14:43:36 +0530

    Created a 3D dungeon using matching squares from 2D texture

---
+++ Assets/Scripts/Tileset.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Tileset", menuName = "Custom/Procedural Generation/Tileset")]
+public class Tileset : ScriptableObject
+{
+    [SerializeField]
+    Color wallColor;
+    [SerializeField]
+    GameObject[] tiles = new GameObject[16];
+
+    public Color WallColor => wallColor;
+    public GameObject GetTile(int tileIndex)
+    {
+        if (tileIndex >= tiles.Length) return null;
+        
+        return tiles[tileIndex];
+    }
+}
